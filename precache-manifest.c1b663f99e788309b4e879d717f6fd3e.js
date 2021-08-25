7ec3f2ff51984e4f1688949a5b1561954bb04066
Patrick <posten.coding@gmail.com>
2021-08-24 20:12:18 -0700

    Updates

---
+++ precache-manifest.c1b663f99e788309b4e879d717f6fd3e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53cf8133ebb2129903aac95a57cfb6e0",
+    "url": "/testHost/index.html"
+  },
+  {
+    "revision": "b9c846f4dfe0964d8b0a",
+    "url": "/testHost/static/css/main.d83db371.chunk.css"
+  },
+  {
+    "revision": "af8d257d33072f3f6cc9",
+    "url": "/testHost/static/js/2.601d76bb.chunk.js"
+  },
+  {
+    "revision": "b9c846f4dfe0964d8b0a",
+    "url": "/testHost/static/js/main.a6adda7f.chunk.js"
+  },
+  {
+    "revision": "35901652b60bf11031da",
+    "url": "/testHost/static/js/runtime-main.aa7adeba.js"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/testHost/static/media/Starjedi.cc3019aa.ttf"
+  },
+  {
+    "revision": "00e5f853f5db44d819027c8803c279ce",
+    "url": "/testHost/static/media/pepsiSpace.00e5f853.png"
+  },
+  {
+    "revision": "800ed3223e2824a1908569da2b881b43",
+    "url": "/testHost/static/media/pepsiSpace1.800ed322.png"
+  }
+]);
